6f6ec52679d4fae8367a0d91b0f53af5724bafe4
rudzkk755 <baharudinbuhari57@gmail.com>
2026-04-25 12:39:57 +0000

    initial commit

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ composer install --optimize-autoloader
 cp .env.example .env || true
 php artisan key:generate
 
-sed -i 's/DB_HOST=127.0.0.1/DB_HOST=mysql/g' .env
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=127.0.0.2/g' .env
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env
 
 php artisan migrate --force
