88319bb68b135a2519fa942ed93ba89a8dc2394e
rudzkk755 <baharudinbuhari57@gmail.com>
2026-04-25 12:36:42 +0000

    initial commit

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+mkdir -p bootstrap/cache \
+         storage/framework/cache \
+         storage/framework/sessions \
+         storage/framework/views
+
+chown -R www-data:www-data bootstrap storage || true
+chmod -R ug+rwx bootstrap storage || true
+
+npm install --legacy-peer-deps --no-audit --progress=false
+npm run dev
+composer install --optimize-autoloader
+cp .env.example .env || true
+php artisan key:generate
+
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=mysql/g' .env
+sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env
+
+php artisan migrate --force
+php artisan db:seed --force
